193d000130bd78b1072de00ec2e4ee341d978802
Terracraft <6617067+terracraft321@users.noreply.github.com>
2026-01-16 12:14:06 +0200

    Add files via upload

---
+++ laskin.py
@@ -0,0 +1,21 @@
+def laskinsum(a, b):
+    """Palauttaa a + b"""
+    return a + b
+
+def laskinerotus(a, b):
+    """Palauttaa a - b"""
+    return a - b
+
+def laskinkerto(a, b):
+    """Palauttaa a * b"""
+    return a * b
+
+def laskinjako(a, b):
+    """Palauttaa a / b, tarkistaa nollajaon"""
+    if b == 0:
+        return "Error: Nollalla jakaminen ei ole sallittua!"
+    return a / b
+
+def laskinpotenssi(a, b):
+    """Palauttaa a ** b"""
+    return a ** b
